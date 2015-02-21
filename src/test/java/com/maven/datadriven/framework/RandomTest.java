a7ce8a264faef097dfc0a02dff750a5d40e8d1c3
deepa <growth.81@gmail.com>
2015-02-21 11:15:41 -0600

    initialupload

---
+++ src/test/java/com/maven/datadriven/framework/RandomTest.java
@@ -0,0 +1,11 @@
+package com.maven.datadriven.framework;
+
+import org.testng.annotations.Test;
+
+public class RandomTest {
+	@Test
+	public void testApp(){
+		System.out.println("inside ---------RANDOM TEST-------------------");
+	}
+
+}
